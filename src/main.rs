423af710a47b02a25496e665760ae36ffd4febfe
Kieran Sweeden <kieransweeden@gmail.com>
2024-01-11 08:52:56 +0000

    remove clap example code

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,10 @@
-use std::path::PathBuf;
-
 use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    /// Optional name to operate on
-    name: Option<String>,
-
-    /// Sets a custom config file
-    #[arg(short, long, value_name = "FILE")]
-    config: Option<PathBuf>,
-
-    /// Turn debugging information on
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
-
     #[command(subcommand)]
-    command: Option<Commands>,
+    command: Commands,
 }
 
 #[derive(Subcommand, Debug)]
@@ -36,38 +23,27 @@ struct BoardArgs {
 
 #[derive(Debug, Subcommand)]
 enum BoardCommands {
-    Create,
+    Create(CreateBoardArgs),
     View,
     Update,
     Delete,
 }
 
+#[derive(Debug, Args)]
+struct CreateBoardArgs {
+    #[arg(short, long)]
+    name: Option<String>,
+
+    #[arg(short, long)]
+    description: Option<String>,
+}
+
 fn main() {
-    let cli = Cli::parse();
+    let args = Cli::parse();
 
-    // You can check the value provided by positional arguments, or option arguments
-    if let Some(name) = cli.name.as_deref() {
-        println!("Value for name: {name}");
-    }
-
-    if let Some(config_path) = cli.config.as_deref() {
-        println!("Value for config: {}", config_path.display());
-    }
-
-    // You can see how many times a particular flag or argument occurred
-    // Note, only flags can have multiple occurrences
-    match cli.debug {
-        0 => println!("Debug mode is off"),
-        1 => println!("Debug mode is kind of on"),
-        2 => println!("Debug mode is on"),
-        _ => println!("Don't be crazy"),
-    }
-
-    // You can check for the existence of subcommands, and if found use their
-    // matches just as you would the top level cmd
-    match &cli.command {
-        Some(Commands::Board(command)) => match command.command {
-            Some(BoardCommands::Create) => {
+    match args.command {
+        Commands::Board(board) => match board.command {
+            Some(BoardCommands::Create(_args)) => {
                 println!("Creating board...");
                 todo!()
             }
@@ -87,8 +63,5 @@ fn main() {
                 println!("Unrecognized command");
             }
         },
-        None => {}
     }
-
-    // Continued program logic goes here...
 }
